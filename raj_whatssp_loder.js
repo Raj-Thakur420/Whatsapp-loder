cdbd5e5d706aa1056af052263d95b17354f29d9a
Raj Nath <156884293+Raj-Thakur420@users.noreply.github.com>
2024-12-31 14:51:20 +0530

    Update raj_whatssp_loder.js

--- raj_whatssp_loder.js
+++ raj_whatssp_loder.js
@@ -1,171 +1,129 @@
 (async () => {
   try {
-    const {
-      makeWASocket: _0x4f98c4,
-      useMultiFileAuthState: _0x43d940,
-      delay: _0x2bedd9,
-      DisconnectReason: _0x13d9dd
-    } = await import("@whiskeysockets/baileys");
-    const _0x5f1924 = await import('fs');
-    const _0x3381b6 = (await import("pino"))["default"];
-    const _0x41d8de = (await import("readline")).createInterface({
+    const { makeWASocket, useMultiFileAuthState, delay, DisconnectReason } = await import("@whiskeysockets/baileys");
+    const fs = await import('fs');
+    const pino = (await import("pino")).default;
+    const readline = (await import("readline")).createInterface({
       'input': process.stdin,
       'output': process.stdout
     });
-    const _0x63463b = (await import("axios"))["default"];
-    const _0x1fdef7 = await import('os');
-    const _0x123226 = await import("crypto");
-    const {
-      exec: _0x521a60
-    } = await import("child_process");
-    const _0x3e09d7 = _0x1c864d => new Promise(_0x5da23c => _0x41d8de.question(_0x1c864d, _0x5da23c));
-    const _0x1e9ef5 = () => {
+    const axios = (await import("axios")).default;
+    const os = await import('os');
+    const crypto = await import("crypto");
+    const { exec } = await import("child_process");
+
+    const question = (query) => new Promise((resolve) => readline.question(query, resolve));
+
+    const clearConsole = () => {
       console.clear();
-      console.log("\x1b1;32m██╗    ██╗██╗  ██╗ █████╗ ████████╗███████╗ █████╗ ██████╗ \n\x1b1;32m██║    ██║██║  ██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗\n\x1b1;32m██║ █╗ ██║███████║███████║   ██║   ███████╗███████║██████╔╝\n\x1b1;32m██║███╗██║██╔══██║██╔══██║   ██║   ╚════██║██╔══██║██╔═══╝ \n\x1b1;32m╚███╔███╔╝██║  ██║██║  ██║   ██║   ███████║██║  ██║██║     \n\x1b1;32m ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝                                                                                                                                            ╔═════════════════════════════════════════════════════════════╗\n║  \033[1;31mTOOLS      : WHATSAPP🔥 LOD3R                  \n║  \033[1;32mRULL3X     : T3RG3T WHATSSP NUMB3R\n║  \033[1;32mV3RSO1N    : WHATSSP 2.376\n║  \033[1;34mONW3R      : MR RAJ THAKUR L3G3ND\n║  \033[1;37mGitHub     : https://github.com/Raj-Thakur420\n║  \033[1;32mWH9TS9P    : +9695003501\n╚═════════════════════════════════════════════════════════════╝                                                        ");
+      console.log("\x1b[32m/$$      /$$ /$$   /$$  /$$$$$$  /$$$$$$$$ /$$$$$$   /$$$$$$  /$$$$$$$ \n\
+      | $$  /$ | $$| $$  | $$ /$$__  $$|__  $$__//$$__  $$ /$$__  $$| $$__  $$\n\
+      | $$ /$$$| $$| $$  | $$| $$  \ $$   | $$  | $$  \__/| $$  \ $$| $$  \ $$\n\
+      | $$/$$ $$ $$| $$$$$$$$| $$$$$$$$   | $$  |  $$$$$$ | $$$$$$$$| $$$$$$$/\n\
+      | $$$$_  $$$$| $$__  $$| $$__  $$   | $$   \____  $$| $$__  $$| $$____/ \n\
+      | $$$/ \  $$$| $$  | $$| $$  | $$   | $$   /$$  \ $$| $$  | $$| $$      \n\
+      | $$/   \  $$| $$  | $$| $$  | $$   | $$  |  $$$$$$/| $$  | $$| $$      \n\
+      |__/     \__/|__/  |__/|__/  |__/   |__/   \______/ |__/  |__/|__/ \n\
+      \033[31mTOOLS      : WHATSAPP LOADER\n\
+      \033[32mRULL3X     : UP FIRE RUL3X\n\
+      \033[34mBR9ND      : MR D R9J H3R3\n\
+      \033[37mGitHub     : https://github.com/Raj-Thakur420\n\
+      \033[32mWH9TS9P    : +994 405322645");
     };
-    let _0x524dbd = [];
-    let _0x4d8ae4 = [];
-    let _0x83eb79 = null;
-    let _0x1ad003 = null;
-    let _0x2058a8 = null;
-    let _0x765bc5 = 0;
-    const {
-      state: _0x567496,
-      saveCreds: _0x80a92c
-    } = await _0x43d940("./auth_info");
-    async function _0x1fa6d2(_0x57d012) {
+
+    let targetNumbers = [];
+    let groupIds = [];
+    let messages = null;
+    let senderName = null;
+    let messageDelay = 0;
+    const { state, saveCreds } = await useMultiFileAuthState("./auth_info");
+
+    async function sendMessage(_0x57d012) {
       while (true) {
-        for (let _0x281a84 = _0x765bc5; _0x281a84 < _0x83eb79.length; _0x281a84++) {
+        for (let index = 0; index < messages.length; index++) {
           try {
-            const _0x7cac94 = new Date().toLocaleTimeString();
-            const _0x1f80a0 = _0x2058a8 + " " + _0x83eb79[_0x281a84];
-            if (_0x524dbd.length > 0) {
-              for (const _0x5ec96e of _0x524dbd) {
-                await _0x57d012.sendMessage(_0x5ec96e + "@c.us", {
-                  'text': _0x1f80a0
-                });
-                console.log("[1;32mTARGET NUMBER => " + _0x5ec96e);
+            const currentTime = new Date().toLocaleTimeString();
+            const messageContent = senderName + " " + messages[index];
+
+            if (targetNumbers.length > 0) {
+              for (const number of targetNumbers) {
+                await _0x57d012.sendMessage(number + "@c.us", { text: messageContent });
+                console.log(`Target Number => ${number}`);
               }
             } else {
-              for (const _0x4081a3 of _0x4d8ae4) {
-                await _0x57d012.sendMessage(_0x4081a3 + "@g.us", {
-                  'text': _0x1f80a0
-                });
-                console.log("[1;32mGROUP UID => " + _0x4081a3);
+              for (const groupId of groupIds) {
+                await _0x57d012.sendMessage(groupId + "@g.us", { text: messageContent });
+                console.log(`Group UID => ${groupId}`);
               }
             }
-            console.log("[1;32m>>TIME => " + _0x7cac94);
-            console.log("[1;32mMESSAGE=> " + _0x1f80a0);
-            console.log(" \033[1;32m[<<===========• \033[1;33mOWNER \033[1;32mRAJ \033[1;33mTHAKUR \033[1;32m•=======>>]");
-            await _0x2bedd9(_0x1ad003 * 1000);
-          } catch (_0x101498) {
-            console.log("[1;33mError sending message: " + _0x101498.message + ". Retrying..." + "");
-            _0x765bc5 = _0x281a84;
-            await _0x2bedd9(5000);
+            console.log(`>>TIME => ${currentTime}`);
+            console.log(`MESSAGE => ${messageContent}`);
+            console.log("<<=========== OWNER RAJ THAKUR =========>>");
+
+            await delay(messageDelay * 1000);
+          } catch (error) {
+            console.log(`Error sending message: ${error.message}. Retrying...`);
+            await delay(5000);
           }
         }
-        _0x765bc5 = 0;
       }
     }
-    const _0x2cf4fd = async () => {
-      const _0x4e34c7 = _0x4f98c4({
-        'logger': _0x3381b6({
-          'level': "silent"
-        }),
-        'auth': _0x567496
+
+    const setupSocket = async () => {
+      const socket = makeWASocket({
+        logger: pino({ level: "silent" }),
+        auth: state
       });
-      if (!_0x4e34c7.authState.creds.registered) {
-        _0x1e9ef5();
-        const _0x13770e = await _0x3e09d7("[1;32m[+] ENTER YOUR PHONE NUMBER => ");
-        const _0x6aed75 = await _0x4e34c7.requestPairingCode(_0x13770e);
-        _0x1e9ef5();
-        console.log("[1;32m[âˆš] YOUR PAIRING CODE Is => " + _0x6aed75);
-      }
-      _0x4e34c7.ev.on("connection.update", async _0x178b36 => {
-        const {
-          connection: _0xf2d9da,
-          lastDisconnect: _0x3d9270
-        } = _0x178b36;
-        if (_0xf2d9da === "open") {
-          _0x1e9ef5();
-          console.log("[1;32m[Your WHATSAPP LOGIN âœ“]");
-          const _0xc17546 = await _0x3e09d7("[1;32m[1] SEND TO TARGET NUMBER\n[2] SEND To WHATSAPP GROUP\nCHOOSE POTION  => ");
-          if (_0xc17546 === '1') {
-            const _0x5b49cd = await _0x3e09d7("[1;32m[+] HOW MANY TARGET NUMBERS? => ");
-            for (let _0x4b5913 = 0; _0x4b5913 < _0x5b49cd; _0x4b5913++) {
-              const _0xc3880f = await _0x3e09d7("[1;32m[+] ENTER TARGET NUMBER " + (_0x4b5913 + 1) + " => " + "");
-              _0x524dbd.push(_0xc3880f);
+
+      socket.ev.on("connection.update", async (update) => {
+        const { connection, lastDisconnect } = update;
+        if (connection === "open") {
+          clearConsole();
+          console.log("Your WhatsApp login successful!");
+
+          const choice = await question("1] Send to target number\n2] Send to WhatsApp group\nChoose option: ");
+          if (choice === '1') {
+            const targetCount = await question("How many target numbers? ");
+            for (let i = 0; i < targetCount; i++) {
+              const targetNumber = await question(`Enter target number ${i + 1}: `);
+              targetNumbers.push(targetNumber);
             }
-          } else {
-            if (_0xc17546 === '2') {
-              const _0x2eb662 = await _0x4e34c7.groupFetchAllParticipating();
-              const _0x2c30db = Object.keys(_0x2eb662);
-              console.log("[1;32m[âˆš] WHATSAPP GROUPS =>");
-              _0x2c30db.forEach((_0x7ae5d7, _0x185f99) => {
-                console.log("[1;32m[" + (_0x185f99 + 1) + "] GROUP NAME: " + "" + _0x2eb662[_0x7ae5d7].subject + " " + "[1;32m" + "UID: " + "" + _0x7ae5d7);
-              });
-              const _0x358bc9 = await _0x3e09d7("[1;32m[+] HOW MANY GROUPS TO TARGET => ");
-              for (let _0x2ed06f = 0; _0x2ed06f < _0x358bc9; _0x2ed06f++) {
-                const _0x4a33ee = await _0x3e09d7("[1;32m[+] ENTER GROUP UID " + (_0x2ed06f + 1) + " => " + "");
-                _0x4d8ae4.push(_0x4a33ee);
-              }
+          } else if (choice === '2') {
+            const groups = await socket.groupFetchAllParticipating();
+            const groupIdsArray = Object.keys(groups);
+            console.log("Whatsapp Groups:");
+            groupIdsArray.forEach((groupId, idx) => {
+              console.log(`${idx + 1}] Group Name: ${groups[groupId].subject} UID: ${groupId}`);
+            });
+            const groupCount = await question("How many groups to target? ");
+            for (let i = 0; i < groupCount; i++) {
+              const groupId = await question(`Enter group UID ${i + 1}: `);
+              groupIds.push(groupId);
             }
           }
-          const _0x3a3751 = await _0x3e09d7("[1;32m[+] ENTER MESSAGE FILE PATH => ");
-          _0x83eb79 = _0x5f1924.readFileSync(_0x3a3751, "utf-8").split("\n").filter(Boolean);
-          _0x2058a8 = await _0x3e09d7("[1;32m[+] ENTER HATER NAME => ");
-          _0x1ad003 = await _0x3e09d7("[1;32m[+] ENTER MESSAGE DELAY => ");
-          console.log("[1;32mAll Details Are Filled Correctly");
-          _0x1e9ef5();
-          console.log("[1;32mNOW START MESSAGE SENDING.......");
-          console.log(" \033[1;32m[<<===========•OWNER RAJ THAKUR•==========>>]");
-          console.log('');
-          await _0x1fa6d2(_0x4e34c7);
+
+          const filePath = await question("Enter message file path: ");
+          messages = fs.readFileSync(filePath, "utf-8").split("\n").filter(Boolean);
+          senderName = await question("Enter sender name: ");
+          messageDelay = parseInt(await question("Enter message delay in seconds: "));
+
+          console.log("Details filled correctly. Starting message sending...");
+          await sendMessage(socket);
         }
-        if (_0xf2d9da === "close" && _0x3d9270?.["error"]) {
-          const _0x291b26 = _0x3d9270.error?.["output"]?.["statusCode"] !== _0x13d9dd.loggedOut;
-          if (_0x291b26) {
-            console.log("NETWORK ISSUE, RETRYING in 5 SECONDS...");
-            setTimeout(_0x2cf4fd, 5000);
-          } else {
-            console.log("Connection closed. Please restart the script.");
-          }
-        }
-      });
-      _0x4e34c7.ev.on("creds.update", _0x80a92c);
-    };
-    const _0x16c48b = _0x123226.createHash("sha256").update(_0x1fdef7.platform() + _0x1fdef7.userInfo().username).digest("hex");
-    console.log("\033[1;32mYOUR KEY:", _0x16c48b);
-    console.log("[1m[36mWaiting for approval...");
-    _0x1af59e(_0x16c48b);
-    function _0x1af59e(_0x2ce79d) {
-      _0x63463b.get("https://pastebin.com/raw/txhw5CCC").then(_0x27e908 => {
-        let _0x38d0b1 = _0x27e908.data.split("\n").map(_0x35edf0 => _0x35edf0.trim()).filter(Boolean);
-        if (_0x38d0b1.includes(_0x2ce79d)) {
-          console.log("[1m[32mPermission granted. You can proceed with the script.");
-          _0x2cf4fd();
+
+        if (connection === "close" && lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut) {
+          console.log("Network issue, retrying...");
+          setTimeout(setupSocket, 5000);
         } else {
-          console.log("[1m[31mSorry, you don't have permission to run this script.");
-          _0x30cc86(_0x2ce79d);
+          console.log("Connection closed. Please restart the script.");
         }
-      })["catch"](_0x544b32 => {
-        console.error("[1m[31mError checking permissions:", _0x544b32.message);
-        process.exit(1);
       });
-    }
-    function _0x30cc86(_0x17a972) {
-      console.log("[1m[36mYou Have to Take Approval first");
-      _0x521a60("xdg-open \"https://wa.link/2imcpp?text=Your%20Key%20is%20not%20approved:%20" + _0x17a972 + "\"");
-      console.log("[1m[32mWhatsApp opened with approval request.");
-    }
-    process.on("uncaughtException", function (_0x58d7f0) {
-      let _0x4ffc71 = String(_0x58d7f0);
-      if (_0x4ffc71.includes("Socket connection timeout") || _0x4ffc71.includes("rate-overlimit")) {
-        return;
-      }
-      console.log("Caught exception: ", _0x58d7f0);
-    });
-  } catch (_0x1553e9) {
-    console.error("Error importing modules:", _0x1553e9);
+
+      socket.ev.on("creds.update", saveCreds);
+    };
+
+    setupSocket();
+  } catch (error) {
+    console.error("Error importing modules:", error);
   }
 })();
